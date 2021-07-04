04647775b4d4b34962a5916a7eab6b69ea0009a7
Jeri Ramadhan <jeriramadhan25@gmail.com>
2021-07-05 00:29:21 +0700

    lagi tambah

--- application/views/menu/indexjs.php
+++ application/views/menu/indexjs.php
@@ -26,7 +26,34 @@
                     })
                 $('.dataTables_filter').append($searchButton, $clearButton);
             }
-        });
+        }),
+    $('#table2').DataTable({
+    'paging': true,
+    'lengthChange': false,
+    // 'searching': true,
+    'ordering': true,
+    'info': true,
+    'autoWidth': true,
+    "language": {
+        "search": "Pencarian"
+    },
+    // initComplete: function() {
+    //     var input = $('.dataTables_filter input').unbind(),
+    //         self = this.api(),
+    //         $searchButton = $('<button>')
+    //         .text('Cari')
+    //         .click(function() {
+    //             self.search(input.val()).draw();
+    //         }),
+    //         $clearButton = $('<button>')
+    //         .text('Hapus')
+    //         .click(function() {
+    //             input.val('');
+    //             $searchButton.click();
+    //         })
+    //     $('.dataTables_filter').append($searchButton, $clearButton);
+    // }
+    });
 
     })
 </script>
